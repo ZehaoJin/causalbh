c2dff56a5168ce21a72a4c2f15b293b20ea15f62
Tristan Deleu <tristan.deleu@gmail.com>
2024-03-18 09:13:26 -0400

    Add scripts

--- exact_posterior/exact_posterior.py
+++ exact_posterior/exact_posterior.py
@@ -13,7 +13,10 @@ def compute_exact_posterior(filename, observations, batch_size=1, verbose=True):
     model = BGe(num_variables=num_variables)
 
     @jax.jit
-    def log_prob(observations, adjacencies):
+    def log_prob(observations, adjacencies_compressed):
+        adjacencies = jnp.unpackbits(adjacencies_compressed, axis=1, count=num_variables ** 2)
+        adjacencies = adjacencies.reshape(-1, num_variables, num_variables)
+
         v_log_prob = jax.vmap(model.log_prob, in_axes=(None, 0))
         log_probs = v_log_prob(observations, adjacencies)
         return jnp.sum(log_probs, axis=1)
@@ -25,17 +28,50 @@ def compute_exact_posterior(filename, observations, batch_size=1, verbose=True):
 
     log_probs = np.zeros((num_dags,), dtype=np.float32)
     for i in trange(0, len(dags_compressed), batch_size, disable=(not verbose)):
-        # Get a batch of DAGs & uncompress
+        # Get a batch of (compressed) DAGs
         batch_compressed = dags_compressed[i:i + batch_size]
-        batch = np.unpackbits(batch_compressed, axis=1, count=num_variables ** 2)
-        batch = batch.reshape(-1, num_variables, num_variables)
 
         # Compute the BGe scores
-        log_probs[i:i + batch_size] = log_prob(observations, batch)
+        log_probs[i:i + batch_size] = log_prob(observations, batch_compressed)
 
     # Normalize the log-marginal probabilities
     log_probs = log_probs - logsumexp(log_probs)
     return log_probs
+
+
+def compute_log_edge_marginal(filename, observations, batch_size=1, verbose=True):
+    num_variables = observations.shape[1]
+    model = BGe(num_variables=num_variables)
+
+    @jax.jit
+    def log_prob(observations, adjacencies_compressed):
+        adjacencies_ = jnp.unpackbits(adjacencies_compressed, axis=1, count=num_variables ** 2)
+        adjacencies = adjacencies_.reshape(-1, num_variables, num_variables)
+
+        v_log_prob = jax.vmap(model.log_prob, in_axes=(None, 0))
+        log_probs = v_log_prob(observations, adjacencies)
+        log_probs = jnp.sum(log_probs, axis=1)
+
+        log_probs_edges = jnp.where(adjacencies_, log_probs[:, None], -jnp.inf)
+        return jax.nn.logsumexp(log_probs_edges, axis=0)
+
+    # Load all the DAGs
+    with open(filename, 'rb') as f:
+        dags_compressed = np.load(f)
+    num_dags = dags_compressed.shape[0]
+
+    log_probs_marginals = []
+    for i in trange(0, len(dags_compressed), batch_size, disable=(not verbose)):
+        # Get a batch of (compressed) DAGs
+        batch_compressed = dags_compressed[i:i + batch_size]
+
+        # Compute the BGe scores
+        log_probs_marginals.append(log_prob(observations, batch_compressed))
+
+    # Normalize the log-marginal probabilities
+    log_probs_marginals = np.stack(log_probs_marginals, axis=0)
+    log_probs_marginals = logsumexp(log_probs_marginals, axis=0) - np.log(num_dags)
+    return log_probs_marginals.reshape(num_variables, num_variables)
 
 
 if __name__ == '__main__':
@@ -43,14 +79,15 @@ if __name__ == '__main__':
     from pathlib import Path
 
     # Random data
-    observations = np.random.normal(size=(100, 6))
+    observations = np.random.normal(size=(35, 7))
 
     root = Path(os.getenv('SLURM_TMPDIR'))
-    log_probs = compute_exact_posterior(
-        root / 'dags.npy',
+    log_probs = compute_log_edge_marginal(
+        root / 'dags_7_final.npy',
         observations,
-        batch_size=512,
+        batch_size=2048,
         verbose=True
     )
-    print(log_probs.shape)
-    print(logsumexp(log_probs))
+
+    with open(root / 'log_probs_edges.npy', 'wb') as f:
+        np.save(f, log_probs)
